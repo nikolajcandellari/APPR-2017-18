17501a43d64abf180ed6abf435a1cdb80f7bdbca
Nikolaj Candellari <nikolajcandellari0@gmail.com>
2017-12-05 17:30:15 +0100

    osnovna uvozna funkcija

---
+++ uvoz/osnovna tabela.R
@@ -0,0 +1,6 @@
+# priprava osnovne tabele
+
+stevilo_intervencij <- read.csv("/U:/CandellariNikolaj/APPR-2017-18/podatki/Pregled stevila intervencij.csv",
+                                 skip =9942)
+
+View(stevilo_intervencij)
